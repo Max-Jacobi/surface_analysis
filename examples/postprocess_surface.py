fix tau units

examples/postprocess_surface.py
@@ -104,12 +104,12 @@ def make_bin_from_centers(c: np.ndarray) -> np.ndarray:
     return b
 
 # copied from runs-thc-ba:analysis/ejecta/skynet/grid.h5
-ej_ye = np.array((0.01, 0.04, 0.07, 0.1, 0.13, 0.16, 0.19, 0.22, 0.25, 
+ej_ye = np.array((0.01, 0.04, 0.07, 0.1, 0.13, 0.16, 0.19, 0.22, 0.25,
     0.29, 0.32, 0.35, 0.38, 0.41, 0.44, 0.47, 0.5))
 ej_s = np.array((1, 1.3, 1.8, 2.4, 3.2, 4.2, 5.6, 7.5, 10, 13, 18, 24,
     32, 42, 56, 75, 100))
-ej_tau = np.array((0.1, 0.17, 0.29, 0.49, 0.84, 1.4, 2.4, 4.2, 7.1, 12, 
-    21, 35, 59, 100, 170, 290, 500))/msol_to_ms
+ej_tau = np.array((0.1, 0.17, 0.29, 0.49, 0.84, 1.4, 2.4, 4.2, 7.1, 12,
+    21, 35, 59, 100, 170, 290, 500))
 
 ejecta_bins = tuple(make_bin_from_centers(ej) for ej in (ej_ye, ej_s, ej_tau))
 
@@ -217,4 +217,4 @@ if args.ejecta:
         hf['mass'] = mass
         hf['Ye'] = ej_ye
         hf['entropy'] = ej_s
-        hf['tau'] = ej_tau
+        hf['tau'] = ej_tau/msol_to_ms